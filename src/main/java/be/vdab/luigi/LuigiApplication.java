96eda8f5e157f246a8bb888f512d2163a0ae7d51
yuliyaBir <zhivo4ka@gmail.com>
2023-07-17 14:03:36 +0200

    Productie op aparte webserver

--- src/main/java/be/vdab/luigi/LuigiApplication.java
+++ src/main/java/be/vdab/luigi/LuigiApplication.java
@@ -2,9 +2,16 @@ package be.vdab.luigi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LuigiApplication {
+public class LuigiApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+        return application.sources(LuigiApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LuigiApplication.class, args);
